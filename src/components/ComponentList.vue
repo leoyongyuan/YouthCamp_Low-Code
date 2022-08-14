7d942e8d0adc6f6446f0d344e1225582af2410a0
Followinsider <ll3670374919@163.com>
2022-08-14 17:26:47 +0800

    fix: 修改ComponentList在打开控制台情况下布局错乱

--- src/components/ComponentList.vue
+++ src/components/ComponentList.vue
@@ -33,7 +33,7 @@ export default {
 <style lang="scss" scoped>
 .component-list {
     height: 65%;
-    padding: 10px;
+    padding: 5px 5px 10px 10px;
     display: grid;
     grid-gap: 10px 19px;
     grid-template-columns: repeat(auto-fill, 80px);
